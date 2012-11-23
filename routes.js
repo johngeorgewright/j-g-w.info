35360e4fd7a80b5476a2cf6f6171d6cf5c7f11b7
John Wright <johngeorge.wright@gmail.com>
2012-11-23 14:02:46 +1100

    Working on portfolio

---
+++ routes.js
@@ -0,0 +1,14 @@
+/*
+ * GET home page.
+ */
+exports.index = function(req, res){
+  res.render('index');
+};
+
+/**
+ * GET protfolio
+ */
+exports.portfolio = function(req, res){
+  res.render('portfolio');
+};
+
