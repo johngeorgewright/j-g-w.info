63303b6fd50692e794c17a6658a1da1e8149671f
John Wright <johngeorge.wright@gmail.com>
2012-11-24 08:11:02 +1100

    Using jake to start the server.

--- app.js
+++ app.js
@@ -50,7 +50,5 @@ app.get('/', routes.index);
 app.get('/portfolio', routes.portfolio);
 app.get('/prices', routes.prices);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
+module.exports = app;
 
